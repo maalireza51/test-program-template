8fa83cb5944a50e8e816f4f4ac004414b46a55c3
maalireza51 <maalireza51@yahoo.com>
2022-08-02 02:12:48 +0430

    diff project

---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+
+// ===================== codemirror configuration for textarea ====================== //
+function codeground(id, text) {
+    var editor = CodeMirror.fromTextArea(document.getElementById(id), {
+        height: "350px",
+        parserfile: "parsexml.js",
+        stylesheet: "css/xmlcolors.css",
+        path: "js/",
+        continuousScanning: 500,
+        lineNumbers: true,
+    });
+    editor.getDoc().setValue(text);
+    var lines = editor.getValue().split('\n');
+    // for (var i = 0; i < lines.length; i++) {
+    //     if (0 < lines[i].indexOf("flag")) {
+    //         editor.getDoc().markText({ line: i, ch: 0 }, { line: i, ch: lines[i].length }, { css: "background-color: yellow" });
+    //     }
+    // }
+}
+
+
+// ===================== if you want to disable a text area ===================//
+//instruction ==> <textarea ... disabled></textare>
+var textarea = document.querySelectorAll('textarea');
+textarea.forEach(element => {
+    if (element.hasAttribute('disabled')) {
+        element.parentNode.querySelector('.disabled').classList.add('d-block');
+    }
+});
+
+// ===================== on page load first diff setup ===================== //
+
+
+var oldText = document.querySelector('.oldText').getAttribute('code');
+var oldName = document.querySelector('.oldText').getAttribute('fileName');
+var newText = document.querySelector('.newText').getAttribute('code');
+var newName = document.querySelector('.newText').getAttribute('fileName');
+
+function defaultdiff(oldName, newName, oldText, newText) {
+    var targetElement = document.getElementById('myDiffElementId');
+    document.addEventListener('DOMContentLoaded', function () {
+
+        var configuration = {
+            drawFileList: false,
+            fileListToggle: false,
+            fileContentToggle: true,
+            matching: 'lines'
+        };
+
+        var diff = Diff.createTwoFilesPatch(oldName, newName, oldText, newText);
+        var diff2htmlUi = new Diff2HtmlUI(targetElement, diff, configuration);
+
+        diff2htmlUi.draw();
+        diff2htmlUi.highlightCode();
+    });
+}
+
+defaultdiff(oldName, newName, oldText, newText);
+
+// ========================= when a button clicked ========================= //
+
+function clickeddiff(oldName, newName, oldText, newText) {
+    var targetElement = document.getElementById('myDiffElementId');
+    targetElement.innerHTML = '';
+
+    var configuration = {
+        drawFileList: false,
+        fileListToggle: false,
+        fileContentToggle: true,
+        matching: 'lines',
+        renderNothingWhenEmpty: false
+    };
+
+    var diff = Diff.createTwoFilesPatch(oldName, newName, oldText, newText);
+    var diff2htmlUi = new Diff2HtmlUI(targetElement, diff, configuration);
+
+    diff2htmlUi.draw();
+    diff2htmlUi.highlightCode();
+}
+
+
+var buttons = document.querySelectorAll('li');
+buttons.forEach(element => {
+    var oldText = element.querySelector('.oldText').getAttribute('code');
+    var newText = element.querySelector('.newText').getAttribute('code');
+    var oldName = element.querySelector('.oldText').getAttribute('fileName');
+    var newName = element.querySelector('.newText').getAttribute('fileName');
+
+    // -------------------- escape and killed color -------------------------- //
+    // var btnMode = false;
+    // var pill = element.querySelector('.pill');
+
+    // const diff = Diff.diffLines(oldText, newText);
+    // diff.some(function (el) {
+    //     if(el.added != undefined || el.removed == true){
+    //         pill.classList.add('pill-red');
+    //         return true;
+    //     }
+    //     else{
+    //         pill.classList.add('pill-green');
+    //         btnMode = true;
+    //     }
+    //     return false
+    // });
+    // -------------------- escape and killed color -------------------------- //
+
+    element.onclick = function () {
+        buttons.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        element.classList.add('active')
+        clickeddiff(oldName, newName, oldText, newText);
+    };
+});
